7edcba84906b3f684c3f367965c7c3d50e8faa63
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2021-02-24 02:44:15 +0100

    test FlashCard & JsonHelper methods in MainActivity

--- app/src/main/java/com/gwesaro/mycheeseornothing/FlashCard.java
+++ app/src/main/java/com/gwesaro/mycheeseornothing/FlashCard.java
@@ -1,6 +1,5 @@
 package com.gwesaro.mycheeseornothing;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
--- app/src/main/java/com/gwesaro/mycheeseornothing/MainActivity.java
+++ app/src/main/java/com/gwesaro/mycheeseornothing/MainActivity.java
@@ -3,12 +3,32 @@ package com.gwesaro.mycheeseornothing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        try {
+            JsonHelper jHelper = new JsonHelper();
+            String str = jHelper.loadJSONFromRes(getBaseContext());
+            JSONObject obj = new JSONObject(str);
+            FlashCard card2 = new FlashCard((JSONObject) obj.getJSONObject("questions").getJSONArray("easy").get(1));
+            Log.i(TAG, card2.toString());
+
+        }catch (Exception e){
+            Log.e(TAG, e.toString());
+        }
     }
+
+
 }
