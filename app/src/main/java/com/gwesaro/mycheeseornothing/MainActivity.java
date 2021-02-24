db5efb8e62157fead914e28b6d8a2b462f22f1d6
RomanCLV <roman.clavier.2001@gmail.com>
2021-02-24 16:26:28 +0100

    home and about done

--- app/src/main/java/com/gwesaro/mycheeseornothing/MainActivity.java
+++ app/src/main/java/com/gwesaro/mycheeseornothing/MainActivity.java
@@ -2,7 +2,9 @@ package com.gwesaro.mycheeseornothing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.aboutButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
 
         String path = "bleu_d_auvergne.jpg";
         String name = path.split("\\.")[0];
