9de63a9c93859fd2c2726bea635519f5cf230610
SarahHayat <saraahyt@gmail.com>
2021-02-25 09:16:26 +0100

    Adapter for questions list

--- app/src/main/java/com/gwesaro/mycheeseornothing/QuestionAdapter.java
+++ app/src/main/java/com/gwesaro/mycheeseornothing/QuestionAdapter.java
@@ -3,6 +3,7 @@ package com.gwesaro.mycheeseornothing;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -11,35 +12,67 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.gwesaro.mycheeseornothing.Question.Question;
+
+import java.util.List;
+
 public class QuestionAdapter extends RecyclerView.Adapter<QuestionAdapter.ViewHolder> implements View.OnClickListener {
+
+    List<Question> questions;
+
+    public QuestionAdapter(List<Question> questions) {
+        this.questions = questions;
+    }
 
     @Override
     public void onClick(View v) {
+        //todo
 
     }
 
     @NonNull
     @Override
     public QuestionAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_questions_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull QuestionAdapter.ViewHolder holder, int position) {
+        Question question = questions.get(position);
+        holder.questionTextView.setText(question.question + "");
+        holder.answer1TextView.setText(question.answers[0] + "");
+        holder.answer2TextView.setText(question.answers[1] + "");
+        holder.answer3TextView.setText(question.answers[2] + "");
+        holder.answer4TextView.setText(question.answers[3] + "");
+       // holder.imageView.setImageResource(this.getResources().getIdentifier(question.imagePath.split("\\.")[0], "drawable", getPackageName()));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return questions.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
+        final TextView questionTextView;
+        final TextView answer1TextView;
+        final TextView answer2TextView;
+        final TextView answer3TextView;
+        final TextView answer4TextView;
+        final ImageView imageView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            questionTextView = itemView.findViewById(R.id.questionTextView);
+            answer1TextView = itemView.findViewById(R.id.answer1TextView);
+            answer2TextView = itemView.findViewById(R.id.answer2TextView);
+            answer3TextView = itemView.findViewById(R.id.answer3TextView);
+            answer4TextView = itemView.findViewById(R.id.answer4TextView);
+            imageView = itemView.findViewById(R.id.questionImageView);
         }
     }
 }
--- app/src/main/java/com/gwesaro/mycheeseornothing/QuestionsListActivity.java
+++ app/src/main/java/com/gwesaro/mycheeseornothing/QuestionsListActivity.java
@@ -1,8 +1,15 @@
 package com.gwesaro.mycheeseornothing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.gwesaro.mycheeseornothing.Question.Question;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuestionsListActivity extends AppCompatActivity {
     QuestionAdapter questionAdapter;
@@ -11,8 +18,14 @@ public class QuestionsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions_list);
-
-        questionAdapter = new QuestionAdapter();
         setTitle("Liste des questions");
+
+        List<Question> questions = new ArrayList<>();
+
+        QuestionAdapter adapter = new QuestionAdapter(questions);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewQuestion);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
